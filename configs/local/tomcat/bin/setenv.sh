2ace749889d897bcde51aa5cd42d6250bae888ee
dnebing <dnebing@gmail.com>
2024-04-22 12:01:25 -0400

    Remove default debug, add debug scripts for optional debug startup

--- configs/local/tomcat/bin/setenv.sh
+++ configs/local/tomcat/bin/setenv.sh
@@ -1,5 +1,6 @@
-CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=768m -XX:MetaspaceSize=768m -XX:NewSize=1536m -XX:SurvivorRatio=7"
+# CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=768m -XX:MetaspaceSize=768m -XX:NewSize=1536m -XX:SurvivorRatio=7"
 
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms4g -Xmx4g -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=768m -XX:MetaspaceSize=768m -XX:NewSize=1536m -XX:SurvivorRatio=7"
 JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=jdk.zipfs/jdk.nio.zipfs=ALL-UNNAMED"
 					
 if [ "$1" = "glowroot" ]
@@ -11,13 +12,10 @@ then
 	shift
 fi
 
-if [ "$GLOWROOT_ENABLED" = "true" ]
-then
+# if [ "$GLOWROOT_ENABLED" = "true" ]
+# then
 	GLOWROOT_OPTS="-javaagent:${CATALINA_HOME}/../glowroot/glowroot.jar"
 
 	CATALINA_OPTS="${CATALINA_OPTS} ${GLOWROOT_OPTS}"
-fi
-					
-##
-## DEBUG
-JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5555"
+# fi
+
