4db048ca45da26de31654a88b7981d3e0e95868b
Erica Gnajek <91756481+egnajek@users.noreply.github.com>
2024-06-01 20:58:59 -0500

    Marked General Info DPT as default
    Marked General Info DPT as default

---
+++ configs/local/tomcat/bin/setenv.sh
@@ -0,0 +1,21 @@
+# CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=768m -XX:MetaspaceSize=768m -XX:NewSize=1536m -XX:SurvivorRatio=7"
+GLOWROOT_ENABLED="true"
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF-8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms4g -Xmx4g -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=768m -XX:MetaspaceSize=768m -XX:NewSize=1536m -XX:SurvivorRatio=7"
+JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=jdk.zipfs/jdk.nio.zipfs=ALL-UNNAMED"
+					
+if [ "$1" = "glowroot" ]
+then
+	GLOWROOT_OPTS="-javaagent:${CATALINA_HOME}/../glowroot/glowroot.jar"
+
+	CATALINA_OPTS="${CATALINA_OPTS} ${GLOWROOT_OPTS}"
+
+	shift
+fi
+
+# if [ "$GLOWROOT_ENABLED" = "true" ]
+# then
+	GLOWROOT_OPTS="-javaagent:${CATALINA_HOME}/../glowroot/glowroot.jar"
+
+	CATALINA_OPTS="${CATALINA_OPTS} ${GLOWROOT_OPTS}"
+# fi
+
