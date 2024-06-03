2337325559897c7bca4d4d9e2500e2fca2546cf7
David Nebinger <david.nebinger@liferay.com>
2024-06-03 14:36:30 +0200

    Add a sleeper context contributor for perf testing.

---
+++ modules/sleeping-context-contributor/src/main/java/com/liferay/enablement/sleeper/context/contributor/SleepingTemplateContextContributor.java
@@ -0,0 +1,36 @@
+package com.liferay.enablement.sleeper.context.contributor;
+
+import com.liferay.portal.kernel.template.TemplateContextContributor;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author dnebinger
+ */
+@Component(
+	property = {"type=" + TemplateContextContributor.TYPE_GLOBAL},
+	service = TemplateContextContributor.class
+)
+public class SleepingTemplateContextContributor
+	implements TemplateContextContributor {
+
+	public class Sleeper {
+           public void sleep(final int seconds) {
+              try {
+              Thread.sleep(seconds * 1000);
+              } catch (Exception e) { }
+           }
+        }
+
+	@Override
+	public void prepare(
+		Map<String, Object> contextObjects, HttpServletRequest request) {
+
+		contextObjects.put("sleeper", new Sleeper());
+	}
+
+}
